5da325c4a82782db48a782e8720e882886f2df4c
Rob de Graaff <rob.degraaff@hotmail.com>
2021-03-29 17:33:18 +0200

    Add project files.

---
+++ XamlSamples/XamlSamples/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamlSamples.Part1;
+using XamlSamples.Part2;
+using XamlSamples.Part3;
+using XamlSamples.Part4;
+
+namespace XamlSamples
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Button button = new Button
+            {
+                Text = "Navigate!",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            button.Clicked += async (sender, args) =>
+            {
+                //await Navigation.PushAsync(new HelloXamlPage());
+                //await Navigation.PushAsync(new XamlPlusCodePage());
+                //await Navigation.PushAsync(new GridDemoPage());
+                //await Navigation.PushAsync(new AbsoluteDemoPage());
+                //await Navigation.PushAsync(new OnPlatformDemoPage());
+                //await Navigation.PushAsync(new SharedResourcesPage());
+                //await Navigation.PushAsync(new StaticConstantsPage());
+                //await Navigation.PushAsync(new RelativeLayoutPage());
+                //await Navigation.PushAsync(new SliderBindingsPage());
+                //await Navigation.PushAsync(new SliderTransformsPage());
+                await Navigation.PushAsync(new ListViewDemoPage());
+            };
+
+            Content = button;
+        }
+    }
+}
---
+++ XamlSamples/XamlSamples/Part1/XamlPlusCodePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamlSamples.Part1
+{
+    public partial class XamlPlusCodePage : ContentPage
+    {
+        public XamlPlusCodePage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            valueLabel.Text = args.NewValue.ToString("F3");
+            //OR
+            //valueLabel.Text = ((Slider)sender).Value.ToString("F3");
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Button button = (Button)sender;
+            await DisplayAlert("Clicked!",
+                "The button labeled '" + button.Text + "' has been clicked",
+                "OK");
+        }
+    }
+}
